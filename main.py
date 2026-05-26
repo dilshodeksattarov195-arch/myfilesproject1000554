4b17f6c13137d7ed052a66d8884501df3e61cc7c
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-26 15:47:33 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class notifyPonnectWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = notifyPonnectWorker()
+    print("Worker engine initialized.")
